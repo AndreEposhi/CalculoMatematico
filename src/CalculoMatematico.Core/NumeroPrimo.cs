d1f33fa141b03c71b32d67877e8188732a953823
André Eposhi <aheposhi@gmail.com>
2021-11-06 16:47:25 -0300

    Add project files.

---
+++ src/CalculoMatematico.Core/NumeroPrimo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CalculoMatematico.Core
+{
+    public static class NumeroPrimo
+    {
+        public static IList<int> ObterNumerosPrimosDeUmNumeroNatural(int numero)
+        {
+            var numerosPrimos = new List<int>();
+
+            if (numero <= 1)
+                return numerosPrimos;
+
+            for (int i = 2; i < numero; i++)
+                if (numero % i == 0)
+                    numerosPrimos.Add(numero);
+
+            return numerosPrimos;
+        }
+    }
+}
