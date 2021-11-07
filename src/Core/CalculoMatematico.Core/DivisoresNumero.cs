614d6553c43a44f75f95ada16c82fed7f305a065
André Eposhi <aheposhi@gmail.com>
2021-11-07 17:39:55 -0300

    Criado as classes para obter o divisor e número primo de um numero natural

---
+++ src/Core/CalculoMatematico.Core/DivisoresNumero.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CalculoMatematico.Core
+{
+    public static class DivisoresNumero
+    {
+        public static IList<int> ObterDivisoresDeUmNumeroNatural(int numero)
+        {
+            var divisoresNumeros = new List<int>();
+
+            if (numero <= 1)
+                return divisoresNumeros;
+
+            for (int num = 1; num <= numero; num++)
+                if (numero % num == 0)
+                    divisoresNumeros.Add(num);
+
+            return divisoresNumeros;
+        }
+    }
+}
---
+++ src/Web/CalculoMatematico.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using CalculoMatematico.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculoMatematico.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index(DivisorViewModel divisor)
+        {
+            return View(divisor);
+        }
+
+        [HttpPost]
+        public IActionResult ObterDivisoresDeUmNumeroNatural(DivisorViewModel divisor)
+        {
+            var divisores = Core.DivisoresNumero.ObterDivisoresDeUmNumeroNatural(divisor.Numero);
+            divisor.Divisores = divisores;
+            divisor.NumerosPrimos = Core.NumeroPrimo.ObterNumerosPrimosDeUmNumeroNatural(divisor.Numero, divisores);
+            return RedirectToAction("Index", divisor);
+        }
+    }
+}
